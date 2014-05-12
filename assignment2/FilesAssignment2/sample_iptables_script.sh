5e6c9b1f65e5665a4bc4b5262a7eaa6a6fee082d
Pilen <fiskomaten@gmail.com>
2014-05-12 22:39:16 +0200

    added assignment2

---
+++ assignment2/FilesAssignment2/sample_iptables_script.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#BIOchem's new firewall
+
+#IP-addresses
+fw_eth1="10.10.1.1"
+fw_eth0="10.10.2.1"
+workstation_net="10.10.1.0/24"
+server_net="10.10.2.0/24"
+mail_srv=...
+
+#Default policies
+...
+
+#Specific rules
+...
+
